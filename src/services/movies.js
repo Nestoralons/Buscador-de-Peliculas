12484cb6c3fdae6c97c009c738e2ff6cec6fe192
Nestoralons <nestoralons42@gmail.com>
2023-06-21 11:52:20 -0400

    Fixed

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,9 +1,10 @@
-const API_KEY = "604b862b";
 export const searchMovies = async ({ search }) => {
   if (search === "") return;
   try {
     const response = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
+      `https://www.omdbapi.com/?apikey=${
+        import.meta.env.VITE_API_KEY
+      }&s=${search}`
     );
     const res = await response.json();
     const Peli = res.Search;
